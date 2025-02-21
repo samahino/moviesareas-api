34418370a61f2604da074b206574d2d22e1cb1ae
samahino <ahino.s1@gmail.com>
2025-02-21 14:36:22 +0530

    -route feature added

--- main.js
+++ main.js
@@ -7,6 +7,20 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hi welcome to express" });
 });
 
+//CRUD functionality of movies
+
+//R- for reading
+app.get("/movies", () => {});
+
+//c- for creating movie
+app.post("/movies", () => {});
+
+//U- for updating
+app.put("/movies/:id", () => {});
+
+//D- for deleting movie
+app.delete("/movies/:id", () => {});
+
 app.listen(PORT, () => {
   console.log(`The server is running at http://localhost:${PORT}`);
 });
