ed483d40c82113f7c829ebe314774ce6426f6c08
samahino <ahino.s1@gmail.com>
2025-02-22 11:12:13 +0530

    -edit, delete code updated

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,7 +1,12 @@
 import Movie from "../models/movie.model.js";
 
-export const MovieIndex = (req, res) => {
-  res.send("Get all movie lists");
+export const MovieIndex = async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export const MovieCreate = async (req, res) => {
@@ -18,10 +23,43 @@ export const MovieCreate = async (req, res) => {
   }
 };
 
-export const MovieUpdate = (req, res) => {
-  res.send("Update a movie");
+export const MovieDetail = async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    if (movie == null) {
+      return res.status(404).json({ message: "can't find movie" });
+    } else {
+      res.json(movie);
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
-export const MovieDelete = (req, res) => {
-  res.send("Delete a movie");
+export const MovieUpdate = async (req, res) => {
+  try {
+    const updateMovie = await Movie.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        title: req.body.title,
+        desc: req.body.desc,
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updateMovie);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const MovieDelete = async (req, res) => {
+  const movieId = req.params.id;
+  try {
+    await Movie.deleteOne({ _id: movieId });
+    res.json({ message: "Movie deleted!" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   MovieCreate,
   MovieDelete,
+  MovieDetail,
   MovieIndex,
   MovieUpdate,
 } from "../controllers/movie.controller.js";
@@ -9,6 +10,8 @@ import {
 const router = express.Router();
 //R- for reading
 router.get("/", MovieIndex);
+
+router.get("/:id", MovieDetail);
 
 //c- for creating movie
 router.post("/", MovieCreate);
